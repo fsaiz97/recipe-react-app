2b18b59f8a7603d4852d3702b3f72c317893268e
fsaiz97 <felixsaiz97@gmail.com>
2022-10-11 11:36:29 +0100

    Add RecipeList component and include in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 // import './App.css'
 import recipes from './recipes'
 // console.log(recipes)
+import {RecipeList} from "./components"
 
 function App() {
     return (
         <div>
             <header>Recipe App</header>
+            <RecipeList recipes={recipes}/>
         </div>
     )
 }
---
+++ src/components/RecipeList/index.jsx
@@ -0,0 +1,12 @@
+function RecipeList({recipes}) {
+    console.log(recipes)
+    return (
+        <div id="recipe-container">
+            <ul className="recipe-list">
+                <li>Bread</li>
+            </ul>
+        </div>
+    )
+}
+
+export default RecipeList;
