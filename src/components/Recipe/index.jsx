90ff87c78fd5a339bdbc33decf258b834c3d5ba5
fsaiz97 <felixsaiz97@gmail.com>
2022-10-11 16:54:03 +0100

    Add ingredients to displayed recipes

--- src/components/Recipe/index.jsx
+++ src/components/Recipe/index.jsx
@@ -7,7 +7,9 @@ function Recipe({recipeData}) {
                 <h2 className='recipe-name'>{recipeData.name}</h2>
                 <p className='recipe-description'>{recipeData.description}</p>
             </div>
-            <ul className='recipe-ingredients'>To-do</ul>
+            <ul className='recipe-ingredients'>
+                {recipeData.ingredients.map((entry, idx) => <li id={`${recipeData.name}-ingredient-${idx}`} className='recipe-ingredient'>{entry}</li>)}
+            </ul>
         </div>
     )
 }
