d8448bb33874ddb3fc09dc476ab6a8fdd42121fe
fsaiz97 <felixsaiz97@gmail.com>
2022-10-11 16:39:59 +0100

    Add css to App Fix component containing div's having id instead of class

--- src/components/Recipe/index.jsx
+++ src/components/Recipe/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Recipe({recipeData}) {
     return (
-        <div id='recipe'>
-            <h2 className='recipe-name'>{recipeData.name}</h2>
-            <p className='recipe-description'>{recipeData.description}</p>
+        <div className='recipe'>
+            <div className='recipe-header'>
+                <h2 className='recipe-name'>{recipeData.name}</h2>
+                <p className='recipe-description'>{recipeData.description}</p>
+            </div>
             <ul className='recipe-ingredients'>To-do</ul>
         </div>
     )
